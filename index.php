64da19289e2f3cc206f494f7a7bae01b6c9d3344
Maciej Gumulka <gg.maciek@gmail.com>
2020-10-25 17:21:19 +0100

    Created header.php

--- index.php
+++ index.php
@@ -2,22 +2,7 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>M. Gumulka</title>
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-  <!-- Google Fonts Roboto -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-  <!-- Bootstrap core CSS -->
-  <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="dist/css/mdb-free.min.css" rel="stylesheet">
-  <!-- Your custom styles (optional) -->
-  <link href="dist/css/style.min.css" rel="stylesheet">
-  <!-- Favicon -->
-  <link rel="shortcut icon" type="image/x-icon" href="dist/img/favicon.ico" />
+  <?php include 'header.php'; ?>
 </head>
 
 <body>
