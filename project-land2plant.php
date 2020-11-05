04822dde65b3e07daba2db55900541e548660c39
Maciej Gumulka <gg.maciek@gmail.com>
2020-11-05 19:53:05 +0100

    Added content to l2p project

--- project-land2plant.php
+++ project-land2plant.php
@@ -26,30 +26,30 @@
       <!-- Container -->
       <div class="container">
         <!-- Section heading -->
-        <h2 class="h1-responsive font-weight-bold text-center mb-2">Land2Plant</h2>
+        <h2 class="h1-responsive font-weight-bold text-center mb-2">My Own Startup in the Energy&Finance Domain</h2>
         <!-- Section description -->
-        <p class="lead grey-text text-justify w-responsive mx-auto mb-5">Land2Plant is a startup I have cofounded together with my three colleagues from studies. We aim to create an international crowdfunding platform, which connects land-owners who want to rent out their properties with citizens who want to invest in a safe and sustainable way. The startup has won several prizes and has been part of several startup incubators. It was an entrepreneurship journey which has taught me a lot. Tons of invested hours, rewarded with business, marketing, finance, law and communication skills one can't simply learn at school.</p>
+        <p class="lead grey-text text-justify w-responsive mx-auto mb-5">Land2Plant is a startup I have co-founded together with my four colleagues from studies. We aim to create an international crowdfunding platform, which connects land-owners who want to rent out their properties with citizens who want to invest in a safe and sustainable way. The startup has won several prizes and has been part of several startup incubators. It was an entrepreneurship journey which has taught me more than any business course can. Tons of invested hours, rewarded with business, marketing, finance, law and communication skills one can't simply learn at school.</p>
 
         <!-- Grid row -->
         <div class="row mb-5">
           <!-- Grid column -->
-          <div class="col-md-7">
-            <i class="fas fa-briefcase fa-5x blue-text animated tada"></i>
+          <div class="col-md-6">
+            <i class="fas fa-handshake fa-5x black-text animated tada"></i>
             <h5 class="font-weight-bold my-4">The Deal</h5>
             <p class="grey-text mb-md-0 mb-0">Throughout the project we learned techniques how to develop simple yet attractive business plans. Our plan was based on a platform model, in which Land2Plant engages with two main stakeholders: landowners and investors (the crowd):
               <li class="grey-text">landowners provide land, investors providide capital,</li>
               <li class="grey-text">Land2Plant investigates project proposals, chooses the most suitable land, designs the power plant and manages the build,</li>
-              <li class="grey-text">investors receive up to 7% ROI,</li>
-              <li class="grey-text">landowners receive part of the money generated from energy sells,</li>
+              <li class="grey-text">Investors receive up to 7% ROI,</li>
+              <li class="grey-text">Landowners receive part of the money generated from energy sells,</li>
               <li class="grey-text">Land2Plant charges up to 20% comissions fee at the start of the project.</li>
             </p>
           </div>
           <!-- Grid column -->
           <!-- Grid column -->
-          <div class="col-md-5">
+          <div class="col-md-6">
             <!--Mask with wave-->
             <div class="view overlay">
-              <img src="dist/img/land2plant-business.png" class="img-fluid rounded" alt="Example image" style="width: 500px; height: 350px">
+              <img src="dist/img/project-l2p-deal.png" class="img-fluid rounded" alt="Example image">
               <a>
                 <div class="mask waves-effect rgba-white-slight"></div>
               </a>
@@ -62,22 +62,21 @@
         <!-- Grid row -->
         <div class="row mb-5">
           <!-- Grid column -->
-          <div class="col-md-7">
-            <i class="fas fa-dollar-sign fa-5x orange-text animated tada"></i>
-            <h5 class="font-weight-bold my-4">Financial Model</h5>
-            <p class="grey-text mb-md-0 mb-4">We developed a robust financial plan, with financial projection for the nearest years. The plan was based on a realistic upscaling plan for our power plants, as well as on assumptions for the annual invested capital. These were determined through an exhaustive benchmarking of our competitors. Naturally, we have also included all fixed and variable expanses. The key challanges which we overcame:
-              <li class="grey-text">making a thorough market research,</li>
-              <li class="grey-text">getting familiar with fixed and variable costs for a bussiness,</li>
-              <li class="grey-text">estimating costs of permits and legal help,</li>
-              <li class="grey-text">making upscaling plans for technical projects, marketing, hiring.</li>
+          <div class="col-md-6">
+            <i class="fas fa-users fa-5x orange-text animated tada"></i>
+            <h5 class="font-weight-bold my-4">Leadership and Project Management</h5>
+            <p class="grey-text mb-md-0 mb-4">In every startup one has to wear many hats. In Land2Plant I had the opportunity to work as Leader, Project Manager and financial planner.
+              <li class="grey-text">I have led a team of 4, ensuring everyone is working on items accordingly with individual skills, mitigating conflicts, leading weekly team meetings and communicating with external stakeholders,</li>
+              <li class="grey-text">As a Project Manager, I was resnponsible for adding structure to our daily work, long and short therm planning, setting long-term goals and organizing technical infrastructure,</li>
+              <li class="grey-text">As a financial planner I developed a long-therm detailed financial plan.</li>
             </p>
           </div>
           <!-- Grid column -->
           <!-- Grid column -->
-          <div class="col-md-5">
+          <div class="col-md-6">
             <!--Mask with wave-->
             <div class="view overlay">
-              <img src="dist/img/land2plant-financials.png" class="img-fluid rounded" alt="Database graph" style="width: 500px; height: 350px">
+              <img src="dist/img/project-l2p-team.jpg" class="img-fluid rounded" alt="Database graph">
               <a>
                 <div class="mask waves-effect rgba-white-slight"></div>
               </a>
@@ -90,22 +89,50 @@
         <!-- Grid row -->
         <div class="row mb-5">
           <!-- Grid column -->
-          <div class="col-md-7">
+          <div class="col-md-6">
+            <i class="fas fa-dollar-sign fa-5x green-text animated tada"></i>
+            <h5 class="font-weight-bold my-4">Financial Model</h5>
+            <p class="grey-text mb-md-0 mb-4">I developed a robust financial plan, with financial projection for the nearest years. The plan was based on a realistic upscaling plan for our power plants, as well as on assumptions for the annual invested capital. These were determined through an exhaustive benchmarking of our competitors. Naturally, we have also included all fixed and variable expanses. The key challanges which we overcame:
+              <li class="grey-text">making a thorough market research,</li>
+              <li class="grey-text">getting familiar with fixed and variable costs for a young bussiness,</li>
+              <li class="grey-text">estimating costs of permits and legal help,</li>
+              <li class="grey-text">making upscaling plans for technical projects, marketing, hiring etc.</li>
+            </p>
+          </div>
+          <!-- Grid column -->
+          <!-- Grid column -->
+          <div class="col-md-6">
+            <!--Mask with wave-->
+            <div class="view overlay">
+              <img src="dist/img/project-l2p-financial.png" class="img-fluid rounded" alt="Database graph" >
+              <a>
+                <div class="mask waves-effect rgba-white-slight"></div>
+              </a>
+            </div>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+
+        <!-- Grid row -->
+        <div class="row mb-5">
+          <!-- Grid column -->
+          <div class="col-md-6">
             <i class="fas fa-user-tie fa-5x grey-text animated tada"></i>
             <h5 class="font-weight-bold my-4">Investor Readiness</h5>
-            <p class="grey-text mb-md-0 mb-0">In order to improve data quality and balance the dataset, I have compared numerous preprocessing techniques and applied the best-performing one. The techniques included:
-              <li class="grey-text">change of performance metrics to Precision&Recall,</li>
-              <li class="grey-text">resampling the data (eg. undersampling and oversampling),</li>
-              <li class="grey-text">generating synthetic data (eg. SMOTE or kNN-SMOTE),</li>
-              <li class="grey-text">trying cost-evaluated or penalized models.</li>
+            <p class="grey-text mb-md-0 mb-0">As part of startup incubators (Sidewalk by Innoenergy and WeWork Labs), we learned what to prepare before talking with investors and business angels:
+              <li class="grey-text">we have built our MVP,</li>
+              <li class="grey-text">we performed competitors analysis,</li>
+              <li class="grey-text">we learned how to realistically set prices for our products,</li>
+              <li class="grey-text">we got experienced in pitching.</li>
             </p>
           </div>
           <!-- Grid column -->
           <!-- Grid column -->
-          <div class="col-md-5">
+          <div class="col-md-6">
             <!--Mask with wave-->
             <div class="view overlay">
-              <img src="dist/img/land2plant-plan.png" class="img-fluid rounded" alt="Example image" style="width: 500px; height: 350px">
+              <img src="dist/img/land2plant-plan.png" class="img-fluid rounded" alt="Example image" >
               <a>
                 <div class="mask waves-effect rgba-white-slight"></div>
               </a>
@@ -119,18 +146,42 @@
         <!-- Grid row -->
         <div class="row mb-5">
           <!-- Grid column -->
-          <div class="col-md-7">
-            <i class="fas fa-trophy fa-5x yellow-text animated tada"></i>
-            <h5 class="font-weight-bold my-4">Awards</h5>
-            <p class="grey-text mb-md-0 mb-0">Our startup has won multiple awards. The journey began by winning the 3rd place at the european finals of EDPR University Challange 2019, having been selected by EDPR Renewables from over 100 ideas. We were then selected to pitch during the global finals. We also achieved the 1st place on the polish EPAE Conferense in the startups cathegory. Then, we were awarded 2nd place during Entreprenourship Journey organized by EIT Innoenergy. Each of these event has taught us how to better communicate bussiness and technical ideas to investors. We mastered out pitching and networking skills, both of which are also important in daily life.
+          <div class="col-md-6">
+            <i class="fas fa-rocket fa-5x red-text animated tada"></i>
+            <h5 class="font-weight-bold my-4">MVP</h5>
+            <p class="grey-text mb-md-0 mb-0">We have built our MVP as part of investor-readiness module. The working app allows for creating profiles with individually chosen investment portfolio. It is also possible to simulate revenue, choose next investments, see educational material and many more.
             </p>
           </div>
           <!-- Grid column -->
           <!-- Grid column -->
-          <div class="col-md-5">
+          <div class="col-md-6">
             <!--Mask with wave-->
             <div class="view overlay">
-              <img src="dist/img/land2plant-awards.jpg" class="img-fluid rounded" alt="Example image" style="width: 500px; height: 350px">
+              <img src="dist/img/project-l2p-app.png" class="img-fluid rounded" alt="Example image" >
+              <a>
+                <div class="mask waves-effect rgba-white-slight"></div>
+              </a>
+            </div>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+
+        <!-- Grid row -->
+        <div class="row mb-5">
+          <!-- Grid column -->
+          <div class="col-md-6">
+            <i class="fas fa-trophy fa-5x yellow-text animated tada"></i>
+            <h5 class="font-weight-bold my-4">Awards</h5>
+            <p class="grey-text mb-md-0 mb-0">Our startup has won multiple awards. The journey began by winning the 3rd place at the european finals of EDPR University Challange 2019, having been selected by EDPR Renewables from over 100 ideas. We were then selected to pitch during the global finals. We also achieved the 1st place on the polish EPAE Conferense in the startups cathegory. Then, we were awarded 2nd place during Entreprenourship Journey organized by EIT Innoenergy. Each of these event has taught us how to better communicate bussiness and technical ideas to investors. We mastered out pitching and networking skills, both of which I consider important in daily life.
+            </p>
+          </div>
+          <!-- Grid column -->
+          <!-- Grid column -->
+          <div class="col-md-6">
+            <!--Mask with wave-->
+            <div class="view overlay">
+              <img src="dist/img/land2plant-awards.jpg" class="img-fluid rounded" alt="Example image" >
               <a>
                 <div class="mask waves-effect rgba-white-slight"></div>
               </a>
